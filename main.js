fb9afc8764d4bf2029cc72ae9a9f0f906599cbf8
iiafn <afnan_twirqi@hotmail.com>
2024-05-22 17:35:41 +0300

    add

---
+++ main.js
@@ -0,0 +1,30 @@
+//1
+for (index=0; index>=-25;index-=2){
+    console.log(index);
+}
+2
+for (index=1; index<=10;index*=2){
+    console.log(index);
+}
+
+// //3
+for (i=1; i<=20;i+=2){
+    console.log(i);
+}
+
+//4
+let number =0
+let num1 =0
+let num2=2
+for (i=0; i<=number;i++){
+    num1=+ num2
+    console.log(i);
+}
+//5
+
+//6
+for (i=){
+    for (){
+
+    }
+}
